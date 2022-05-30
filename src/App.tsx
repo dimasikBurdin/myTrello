546164c541906cbc2dfae04cdd447fc699b5daf9
dimasikBurdin <81648493+dimasikBurdin@users.noreply.github.com>
2022-05-30 22:33:40 +0500

    логика перемещения контейнера перемещена на уровень самой "доски"
    это пофиксило баги при перетаскивании

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Container } from './components/container';
+
+type CardInfo = {
+  title: string
+  description: string
+  color: string
+  id: number
+}
 
 function App() {
   const [leftSideElements, setLeftSideElements] = useState(['green', 'red', 'yellow', 'blue']);
@@ -10,7 +17,50 @@ function App() {
   const [mouseMoveEvent, setMouseMoveEvent] = useState<React.MouseEvent<HTMLDivElement, MouseEvent>>();
   const [isMouseUp, setIsMouseUp] = useState(true);
 
+  const activeCard = useRef<HTMLElement>();
+  const cursorPositionEvent = useRef<React.MouseEvent<HTMLDivElement, MouseEvent>>();
+  const [delta, setDelta] = useState<{left: number, top: number}>();
 
+  function setActiveCard(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    let el = e.target as HTMLElement;
+    activeCard.current = el;
+    let current = activeCard.current;
+
+    let deltaLeft = e.pageX - current.getBoundingClientRect().left;
+    let deltaTop = e.pageY - current.getBoundingClientRect().top;
+
+    current.style.position = 'absolute';
+    current.style.zIndex = '1000';
+
+    current.style.left = (e.pageX - deltaLeft)  + 'px';
+    current.style.top = (e.pageY - deltaTop) + 'px';
+
+    setDelta({left: deltaLeft, top: deltaTop});
+  }
+
+  function onDrag(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    if(!activeCard.current) return;
+    console.log('drag')
+    let pX = e.pageX;
+    let pY = e.pageY;
+
+    if(pX)
+      activeCard.current.style.left = (pX - delta.left)  + 'px';
+    if(pY)
+      activeCard.current.style.top = (pY - delta.top) + 'px';
+  }
+
+  function endDrag() {
+    if(!activeCard.current) return;
+    activeCard.current.style.position = 'relative';
+    activeCard.current.style.top = '0';
+    activeCard.current.style.left = '0';
+    activeCard.current.style.zIndex = '0';
+
+    activeCard.current = null;
+  }
+
+  
   function dragOver(e: React.DragEvent<HTMLDivElement>) {
     e.preventDefault();
   }
@@ -18,55 +68,73 @@ function App() {
   function onDropRight(e: React.DragEvent<HTMLDivElement>) {
     console.log('drop to right');
 
-    let currentColor = e.dataTransfer.getData('text').split(' ')[1];
-    if(!rightSideElements.includes(currentColor)) {
-      setRightSideElements([...rightSideElements, currentColor]);
+    // let currentColor = e.dataTransfer.getData('text').split(' ')[1];
+    // if(!rightSideElements.includes(currentColor)) {
+    //   setRightSideElements([...rightSideElements, currentColor]);
 
-      let ind = leftSideElements.indexOf(currentColor);
-      if(ind !== -1) {
-        let temp = [...leftSideElements];
-        temp.splice(ind, 1);
-        setLeftSideElements(temp);
-      }
-    }
+    //   let ind = leftSideElements.indexOf(currentColor);
+    //   if(ind !== -1) {
+    //     let temp = [...leftSideElements];
+    //     temp.splice(ind, 1);
+    //     setLeftSideElements(temp);
+    //   }
+    // }
   }
 
   function onDropLeft(e: React.DragEvent<HTMLDivElement>) {
     console.log('drop to left')
 
-    let currentColor = e.dataTransfer.getData('text').split(' ')[1];
-    if(!leftSideElements.includes(currentColor)) {
-      setLeftSideElements([...leftSideElements, currentColor]);
+    // let currentColor = e.dataTransfer.getData('text').split(' ')[1];
+    // if(!leftSideElements.includes(currentColor)) {
+    //   setLeftSideElements([...leftSideElements, currentColor]);
 
-      let ind = rightSideElements.indexOf(currentColor);
-      if(ind !== -1) {
-        let temp = [...rightSideElements];
-        temp.splice(ind, 1);
-        setRightSideElements(temp);
-      }    
-    }
+    //   let ind = rightSideElements.indexOf(currentColor);
+    //   if(ind !== -1) {
+    //     let temp = [...rightSideElements];
+    //     temp.splice(ind, 1);
+    //     setRightSideElements(temp);
+    //   }    
+    // }
   }
 
   return (
-    <div className='app-container' onMouseMove={e => setMouseMoveEvent(e)} onMouseUp={() => setIsMouseUp(true)} onMouseDown={() => setIsMouseUp(false)}>
+    <div className='app-container' 
+      // onMouseMove={e => setMouseMoveEvent(e)}
+      onMouseMove={e => onDrag(e)}
+      // onMouseUp={() => {setIsMouseUp(true); setMouseMoveEvent(null); console.log('mouseUp in left side')}}
+      onMouseUp={() => endDrag()}
+      onMouseDown={() => setIsMouseUp(false)} 
+      onDragStart={() => false}
+    >
       <div className='left-side' 
-        // onDragOver={dragOver} 
-        // onDrop={e => onDropLeft(e)}
+        onDragStart={() => false}
+        draggable='false'
       >
-      {leftSideElements.map(color => {          
-          return <Container key={color} color={color} mouseMoveEvent={mouseMoveEvent} isMouseUp={isMouseUp} />;
+        {leftSideElements.map(color => {          
+          return <div key={color} className='left-side-card-container'>
+              <Container 
+                // key={color}  
+                color={color} 
+                setActiveCard={setActiveCard}
+              />
+            </div>            
         })}
 
-      {/* <Container color={'green'}/> */}
       </div>
-      {/* <div className='right-side' 
-        // onDragOver={dragOver} 
-        // onDrop={e => onDropRight(e)}
+      <div className='right-side'
+        onDragStart={() => false}
+        draggable='false'
+        onMouseUp={e => console.log('mouseUp in right side')}
+        style={{zIndex:"1"}}
       >
         {rightSideElements.map(color => {          
-          return <Container key={color} color={color}/>;
+          return <Container 
+              key={color} 
+              color={color} 
+              setActiveCard={setActiveCard}
+            />;
         })}
-      </div> */}
+      </div>
       
     </div>
     
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,51 +3,15 @@ import './container.css';
 
 type TProps = {   
     color: string,
-    mouseMoveEvent: React.MouseEvent<HTMLDivElement, MouseEvent> | undefined,
-    isMouseUp: boolean
+    setActiveCard: Function
 }
 
 export const Container:React.FC<TProps> = React.memo((props) => {
-    const [activeElement, setActiveElement] = useState<HTMLElement>();
-    const [delta, setDelta] = useState({left: Number(), top: Number()});
-
-    function onDragStart(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-        let el = e.target as HTMLElement;
-
-        let deltaLeft = e.pageX - el.getBoundingClientRect().left;
-        let deltaTop = e.pageY - el.getBoundingClientRect().top;
-
-        el.style.position = 'absolute';
-
-        setActiveElement(el);
-        setDelta({left: deltaLeft, top: deltaTop});
-    }
-
-    useEffect(() => {
-        if(!activeElement) return;
-        if(activeElement && (props.mouseMoveEvent?.pageX || props.mouseMoveEvent?.pageY)) {
-            let pX = props.mouseMoveEvent?.pageX as number;
-            let pY = props.mouseMoveEvent?.pageY as number;
-
-            activeElement.style.left = (pX - delta.left)  + 'px';
-            activeElement.style.top = (pY - delta.top) + 'px';
-
-            console.log('active')
-        }
-
-        return () => {return}
-    }, [activeElement, props.mouseMoveEvent?.pageX, props.mouseMoveEvent?.pageY]);
-
-    useEffect(() => {
-        if(!props.isMouseUp || !activeElement) return;
-        // activeElement.style.position = 'relative';
-        setActiveElement(undefined);
-        setDelta({left: 0, top: 0});
-    }, [props.isMouseUp]);
-
     return <div className={`container ${props.color}`}
-                onMouseDown={e => onDragStart(e)}
-            >
+            onMouseDown={e => props.setActiveCard(e)}
+            onDragStart={() => false}
+            draggable='false'
+        >
     </div>
 })
     
